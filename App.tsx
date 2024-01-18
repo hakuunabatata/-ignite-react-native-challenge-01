788da1f1bc0d805dc3f8b8b28bdca7d8b02ae12f
hakuunabatata <emachadomartins1999@gmail.com>
2024-01-17 21:19:21 -0300

    create basic structure

--- App.tsx
+++ App.tsx
@@ -1,13 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+
+interface Task {
+  task: string
+  done: boolean
+}
 
 export default function App() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [value, setValue] = useState('')
+
+  const addTask = () => {
+    setTasks(prev => [...prev, { task: value, done: false }])
+  }
+
+  const removeTask = (index: number) => {
+    setTasks(prev => prev.filter((_, idx) => idx !== index))
+  }
+
+  const endTask = (index: number) => {
+    setTasks(prev =>
+      prev.map((item, idx) =>
+        idx === index ? { task: item.task, done: !item.done } : item,
+      ),
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <View style={styles.header}>
+        <Text>Logo</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+      />
+      <TouchableOpacity onPress={addTask}>
+        <Text>Create</Text>
+      </TouchableOpacity>
+      <View>
+        <Text>Criadas</Text>
+        <Text>{tasks.length}</Text>
+      </View>
+      <View>
+        <Text>Concluídas</Text>
+        <Text>{tasks.filter(({ done }) => done).length}</Text>
+      </View>
+      {tasks.map((task, index) => (
+        <View key={index}>
+          <TouchableOpacity onPress={() => endTask(index)}>
+            <Text>Concluir</Text>
+          </TouchableOpacity>
+          <Text>{task.task}</Text>
+          <TouchableOpacity onPress={() => removeTask(index)}>
+            <Text>Delete</Text>
+          </TouchableOpacity>
+        </View>
+      ))}
+      <StatusBar style='auto' />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +76,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  header: {
+    backgroundColor: '#f00',
+  },
+  input: {
+    backgroundColor: '#0ee',
+    color: '#000',
+  },
+})
